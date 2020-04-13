08e351f4ecc798f7119e7337e4e9f3d8a93d56b0
RobWongus <59811683+RobWongus@users.noreply.github.com>
2020-04-13 13:48:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 //creating a server/ port  
+
+
+//Dependencies
 const express = require("express");
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
@@ -7,15 +10,17 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 //change PORT number for new projects
 //const PORT = process.env.PORT || 3000; 
-
+//sets the Express app to handle data parsing
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-//connecting back in routes to html and api pages
+//connecting back in routes to html and api pages, server
 app.use("/api", apiRoutes);  
 app.use("/", htmlRoutes);    
+
+
 
 app.listen(PORT, function(){
     console.log("Listening on PORT: " + PORT);
